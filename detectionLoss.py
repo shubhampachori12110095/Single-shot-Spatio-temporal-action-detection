9026bfda398680c3cd505e6ca6a92d93f5c41922
aelnouby <aelnouby@WS7.mlrg.soe.uoguelph.ca>
2017-09-19 15:14:19 -0400

    adding loss function initial verion

--- detectionLoss.py
+++ detectionLoss.py
@@ -7,12 +7,24 @@ class DetectionLoss(torch.nn.Module):
     Detection loss function.
     """
 
-    def __init__(self, iouThresh=0.5):
+    def __init__(self, num_classes, num_boxes, grid_size, image_size, iouThresh=0.5, coord_scale = 5, noobj_scale = 0.5):
         super(DetectionLoss, self).__init__()
+        self.num_classes = num_classes
+        self.num_boxes = num_boxes
         self.iouThresh = iouThresh
+        self.coord_scale = coord_scale
+        self.noobj_scale = noobj_scale
+        self.grid_size = grid_size
+        self.image_size = image_size
 
     def forward(self, output, target):
-        pass
+        class_confidence = output[:, :, :, :self.num_classes]
+        region_confidence = output[:, :, :, self.num_classes : self.num_classes + self.num_boxes]
+        predicted_bbox = output[:, :, :, self.num_classes + self.num_boxes:]
+
+        tx, ty, tw, th, tc = target
+        cell_x = torch.floor(tx / (self.image_size / self.grid_size))
+        cell_y = torch.floor(ty / (self.image_size / self.grid_size))
 
 
     def iou(self, bbox1, bbox2):
